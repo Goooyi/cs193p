93dee94e0345ba7ea2a64b9a5db44b437513e1d1
Goooyi <gaaoyi@gmail.com>
2023-11-08 21:33:57 +0800

    Initial commit

---
+++ memorizer/memorizer/memorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizerApp.swift
+//  memorizer
+//
+//  Created by Yi Gao on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct memorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
